9d8d1de529260f8434a2aae4dedbd3dd38576a43
Stephen H. Gerstacker <stephen@gerstacker.us>
2024-12-02 12:10:32 -0500

    Day 02, Both Parts

---
+++ Advent of Code/Day2.swift
@@ -0,0 +1,66 @@
+//
+//  Day2.swift
+//  Advent of Code 2024
+//
+//  Created by Stephen H. Gerstacker on 2024-12-02.
+//  SPDX-License-Identifier: MIT
+//
+
+import Foundation
+
+public struct Day2: AdventDay {
+    
+    struct Report {
+        var levels: [Int]
+        
+        var isSafe: Bool {
+            let diffs = levels.indices.dropFirst().map { levels[$0] - levels[$0 - 1] }
+            
+            let allDecrease = diffs.allSatisfy { $0 <= 0 }
+            let allIncrease = diffs.allSatisfy { $0 >= 0 }
+            
+            guard allDecrease || allIncrease else { return false }
+            
+            let allDiffer = diffs.allSatisfy { abs($0) > 0 && abs($0) <= 3 }
+            
+            return allDiffer
+        }
+        
+        var isSafeDampened: Bool {
+            guard !isSafe else { return true }
+            
+            for index in levels.indices {
+                var nextLevels = levels
+                nextLevels.remove(at: index)
+                
+                let nextReport = Report(levels: nextLevels)
+                
+                if nextReport.isSafe { return true }
+            }
+            
+            return false
+        }
+    }
+    
+    public var data: String
+    
+    var reports: [Report] {
+        data
+            .split(separator: "\n")
+            .map { $0.split(separator: " ") }
+            .map { $0.compactMap { Int($0) } }
+            .map { Report(levels: $0) }
+    }
+    
+    public init(data: String) {
+        self.data = data
+    }
+    
+    public func part1() async throws -> Any {
+        reports.filter(\.isSafe).count
+    }
+    
+    public func part2() async throws -> Any {
+        reports.filter(\.isSafeDampened).count
+    }
+}
